6c2a1d3555a5285f3df0f30b319b60d6fe9ed4d5
= <pete@daffodil.uk.com>
2011-08-21 18:26:05 +0100

    config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+/* This is the main MP config for the Javascript */
+
+//* This is the key for localhost
+var GMAP_KEY = "ABQIAAAA1BTgxTzKI7pnrfPzp4v8CBT2yXp_ZAY8_ufC3CFXhHIE1NvwkxQyAnBpfeDDAGILvn7ComCbDKmJsA";
+
+//* Replace the value below with your own key domain.com key
+//var GMAP_KEY = "< -- your key here  -- >";
+
+var GMAP_VER = 2.66;
